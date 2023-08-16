8b48d6c23794008dd9854483d6a988caf6f9870d
zuhayrtariq <zuhayrtariq4@gmail.com>
2023-08-16 22:57:28 +0500

    Completed Front End

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+
+import luckycement from './company/luckycement.png';
+import tfmdigital from './company/tfmdigital.png';
+import prime from './company/prime.png';
+import ezsolvers from './company/ezsolvers.png';
+
+import uniboard from './uniboard.png';
+import airbnb from './airbnb.png';
+import cinemate from './cinemate.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  luckycement,
+  prime,
+  ezsolvers,
+  tfmdigital,
+  uniboard,
+  airbnb,
+  cinemate,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,269 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  luckycement,
+  tfmdigital,
+  prime,
+  ezsolvers,
+  threejs,
+  uniboard,
+  airbnb,
+  cinemate,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Web Developer',
+    icon: web,
+  },
+  {
+    title: 'Web Designer',
+    icon: mobile,
+  },
+  {
+    title: 'Backend Engineer',
+    icon: backend,
+  },
+  {
+    title: 'Content Creator',
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Information Technology Intern',
+    company_name: 'Lucky Cement Ltd.',
+    icon: luckycement,
+    iconBg: 'white',
+    date: 'November 2021 - January 2022',
+    points: [
+      'Worked on SAP ABAP',
+      'Writing queries in ABAP to perform CRUD Operations on the database',
+      `Worked on generating reports, building report interfaces and modularizing programs with 'Includes', 'Forms' and 'Function Modules'`,
+    ],
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'TFM Digital',
+    icon: tfmdigital,
+    iconBg: '#E6DEDD',
+    date: 'Aug 2022 - Jan 2023',
+    points: [
+      'Worked remotely for TFM Digital',
+      'Performing duties of both front end and back end developer.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Web Developer',
+    company_name: 'EZ Solvers',
+    icon: ezsolvers,
+    iconBg: 'white',
+    date: 'Feb 2023 - June 2023',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Graduate Trainee Officer',
+    company_name: 'Prime Pakistan',
+    icon: prime,
+    iconBg: '#E6DEDD',
+    date: 'July 2023 - Present',
+    points: [
+      'Working at a GTO',
+      'Looking after day to day activities in the IT Department',
+      'Dealing with technical support, providing aid to members of the team and managers as required.',
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'Zuhayr was exceptional to work with. The communication was great and he helped me navigate web development with my limited experience.',
+    name: 'Taylor Fielding',
+    designation: 'CEO',
+    company: 'TFM Digital',
+    image:
+      'https://www.mediaweek.com.au/wp-content/uploads/2023/06/Taylor-Fielding-tfm.digital-.jpg',
+  },
+  {
+    testimonial:
+      'He is a hard worker who is extremely dedicated towards the task and is an excellent problem solver.',
+    name: 'Salman Agha',
+    designation: 'Employee',
+    company: 'Lucky Cement',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      'A skilled web developer, who helped us in developing our website. Very cooperative and great problem solving skills',
+    name: 'Hammad Siddiqui',
+    designation: 'Founder',
+    company: 'EZ Solvers',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFJ-V68JYIToXOlu9PuG3W0NtA3L0KzWdrJzgMjExJVA&s',
+  },
+];
+
+const projects = [
+  {
+    name: 'Uniboard (UMS)',
+    description:
+      'A University Management System that deals with all the routine tasks performed by Students, Faculty, Administrators and Head of Departments. Main technologies used were NodeJS, ExpressJS, EJS, MySQL',
+    tags: [
+      {
+        name: 'NodeJS',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'MySQL',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'EJS',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: uniboard,
+    source_code_link: 'https://github.com/zuhayrtariq/FYP',
+  },
+  {
+    name: 'AirBnb - Booking App',
+    description:
+      'A Full-Stack booking app built using MERN(Mongo, Express, React, Node). The design and the idea was inspired by AirBnb. Front-end built using React and Tailwindcss. Bakcend based on Node.',
+    tags: [
+      {
+        name: 'React',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'RESTAPI',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'TailwindCSS',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'MongoDB',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: airbnb,
+    source_code_link: 'https://github.com/zuhayrtariq/airbnb-clone',
+  },
+  {
+    name: 'Cinemate Movie App',
+    description:
+      'A React Application built with the help of TMDB API. The App allows you to search for a movie and check the rating and the details. Allows user to toggle dark and light mode as well.',
+    tags: [
+      {
+        name: 'ReactJS',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'API',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cinemate,
+    source_code_link: 'https://github.com/zuhayrtariq/cinemate-app',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
